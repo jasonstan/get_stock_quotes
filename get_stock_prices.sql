d8053d4911a09bfa88113e5a1e13af56b2d1855a
jasonstan <stanley.jason@gmail.com>
2016-10-15 21:28:41 -0400

    Change default created_at timestamp to now at utc timezone

--- get_stock_prices.sql
+++ get_stock_prices.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS company, stock_prices;
 -- Create company table to store slowly changing fields for each company
 CREATE TABLE company(
     id serial not null primary key,
-    created_at timestamp not null default now(),
+    created_at timestamp not null default (now() at time zone 'utc'),
     stock_symbol varchar(20) default null,
     company_name varchar(50) default null,
     index varchar (20) default null
@@ -15,7 +15,7 @@ CREATE TABLE company(
 -- Create stock_prices table to store regular updates on stock price
 CREATE TABLE stock_prices(
     id serial not null primary key,
-    created_at timestamp not null default now(),
+    created_at timestamp not null default (now() at time zone 'utc'),
     stock_symbol varchar(20) default null,
     last_traded_price money default null,
     last_trade_datetime timestamp default null
